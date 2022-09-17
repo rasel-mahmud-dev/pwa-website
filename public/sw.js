ccb971159394bf63da598d891ecd584fb3664091
rasel mahmud dev <rasel.mahmud.dev@gmail.com>
2022-09-17 17:34:00 +0600

    remove redundant files

--- public/sw.js
+++ public/sw.js
@@ -55,21 +55,24 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
     // if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
     // console.log(evt.request.url);
-    // evt.respondWith(
-    //     caches.match(evt.request).then(cacheRes => {
-    //         return cacheRes || fetch(evt.request).then(fetchRes => {
-    //             return caches.open(dynamicCacheName).then(cache => {
-    //                 cache.put(evt.request.url, fetchRes.clone());
-    //                 // check cached items size
-    //                 limitCacheSize(dynamicCacheName, 15);
-    //                 return fetchRes;
-    //             })
-    //         });
-    //     }).catch(() => {
-    //         if (evt.request.url.indexOf('.html') > -1) {
-    //             return caches.match('/pages/fallback.html');
-    //         }
-    //     })
-    // );
+
+    // serve caching page
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    // check cached items size
+                    limitCacheSize(dynamicCacheName, 15);
+                    return fetchRes;
+                })
+            });
+        }).catch(() => {
+            if (evt.request.url.indexOf('.html') > -1) {
+                return caches.match('/pages/fallback.html');
+            }
+        })
+    );
+
     // }
 });
